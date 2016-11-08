5e679170de56b3ba6b3e056a6bda770771ac049e
Aris Supriyanto <arissy96@gmail.com>
2016-11-08 13:56:50 +0700

    test

--- app/hello/__init__.py
+++ app/hello/__init__.py
@@ -32,8 +32,9 @@ def addmember():
         datajs = {'token':acakpass,'password':passwordhash,'acess_token':access_token,'public_key':public_key}
         userprofile = {"email":loadjson['email'],"phone":loadjson['phone'],"name":loadjson['name']}
         user_profile = json.dumps(userprofile)
-        
+
         try:
+            
             data = od_users(
                 username=loadjson['username'],
                 password=passwordhash,
