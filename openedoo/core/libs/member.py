899ce86fdc0d2e665452a8dcba430a9ee39f418f
rendiya <ligerrendy@gmail.com>
2016-11-15 14:53:15 +0700

    push master

--- openedoo/core/libs/member.py
+++ openedoo/core/libs/member.py
@@ -1,24 +1,22 @@
-from flask import abort, request, Response
 from datetime import datetime, timedelta
 from openedoo.core.libs.db import query
 from openedoo.core.libs.db.db_tables import od_users
 from openedoo.core.libs.tools import randomword,hashingpw,cocokpw,setredis,getredis,hashingpw2,checkpass2
 import json
-from flask import abort
 
-now = datetime.now()
-sekarang = now.strftime('%Y-%m-%d %H:%M:%S')
+now_temp = datetime.now()
+now = now_temp.strftime('%Y-%m-%d %H:%M:%S')
 
 query = query()
 
 def registration(username, password, email, name, phone):
-    userprofile = {"email":email,"phone":phone,"name":name}
-    user_profile = json.dumps(userprofile)
+    user_profile = {"email":email,"phone":phone,"name":name}
+    user_profile_json = json.dumps(user_profile)
     public_key = 'NULL'
     private_key = 'NULL'
 
-    acakpass = (randomword(16)+password)
-    passwordhash = hashingpw2(password)
+    acak_pass = (randomword(16)+password)
+    password_hash = hashingpw2(password)
     access_token = hashingpw(acakpass)
     try:
         data = od_users(
@@ -29,23 +27,20 @@ def registration(username, password, email, name, phone):
             private_key=private_key,
             status=1,
             role='0',
-            created=sekarang,
-            last_login=sekarang,
-            user_profile=str(user_profile)
+            created=now,
+            last_login=now,
+            user_profile=str(user_profile_json)
         )
-        try:
-            query.insert_db(new=data)
-        except Exception as e:
-            raise e
-    except Exception as e:
-        raise e
-    return user_profile
+        query.insert_db(new=data)
+    except Exception:
+        return False
+    return user_profile_json
 
 def login(username, password):
     try:
         data = query.selectdb(od_users,None, None)
         if len(data) < 1:
-            abort(403)
+            return False
         return data
     except Exception as e:
-        return "Error"
+        return False
