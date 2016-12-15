38f4238ef275ca4ab0fd2dd80813f4f3a91e2bb6
rendiya <ligerrendy@gmail.com>
2016-12-15 10:28:02 +0700

    fix add modul

--- openedoo/core/libs/get_modul.py
+++ openedoo/core/libs/get_modul.py
@@ -24,12 +24,15 @@ def check_modul_available(url="https://api.github.com/orgs/openedoo/repos"):
 		jumlah = (number_awal+1)-1
 		modul = data[jumlah]
 		cek_modul = data[jumlah]['name']
-		if 'modul_' in cek_modul:
+		if 'modul' in cek_modul:
 			modul_url = modul['contents_url']
 			modul_url_original = modul_url.replace("{+path}", "")
 			modul_url_requirement = modul_url.replace("{+path}", "requirement.json")
 			modul_name = modul['name']
-			modul_list = {'name':modul_name,'url':modul_url_original,'url_requirement':modul_url_requirement}
+			modul_git = modul['clone_url']
+			modul_list = {'name':modul_name,'url':modul_url_original,\
+			'url_requirement':modul_url_requirement,\
+			'url_git':modul_git}
 			list_all.append(modul_list)
 		else:
 			pass
@@ -56,12 +59,12 @@ def find_modul(modul_name=None):
 	for number_awal in xrange(number_awal,number_akhir):
 		jumlah = (number_awal+1)-1
 		if modul_name in data[jumlah]['name']:
-			output = {'url':data[jumlah]['url'],'url_requirement':data[jumlah]['url_requirement']}
+			output = {'url':data[jumlah]['url'],'url_requirement':data[jumlah]['url_requirement'],\
+			'url_git':data[jumlah]['url_git'],'name':data[jumlah]['name']}
 			return output
 		else:
 			pass
 	return output
-
 def install_git(url=None,directory=None,name_modul=None):
 	if url == None:
 		return "your url is None"
