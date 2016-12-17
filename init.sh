38936b4dd8edd346a01ec48a600336634d66949c
anak10thn <anak10thn@gmail.com>
2016-12-17 19:36:48 +0700

    UPDATE: waiting connection

--- init.sh
+++ init.sh
@@ -3,6 +3,11 @@ cd /opt/od
 CMDDOCK=$@
 if [ -z "$CMDDOCK" ]
 then
+  while ! nc -z oddb 3306
+  do
+    echo "Waiting Database Connection.......!!!"
+    sleep 1
+  done
   if [ ! -f "/opt/od/config.json" ]
   then
     cat /opt/od/config.json.example | sed "s|your_username|root|g" | sed "s|your_password|pass123|g" | sed "s/localhost/oddb/g" > /opt/od/config.json
