d58c8b669e74e2cb347dbe492736afd0d53bb68e
anak10thn <anak10thn@gmail.com>
2016-12-17 19:12:28 +0700

    UPDATE: entrypoint run

--- init.sh
+++ init.sh
@@ -9,9 +9,9 @@ then
     python manage.py db init
     python manage.py db migrate
     python manage.py db upgrade
-    python manage.py runserver
+    python manage.py run
   else
-    python manage.py runserver
+    python manage.py run
   fi
 else
   $@
