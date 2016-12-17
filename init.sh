42e47836c25a070d57d62f9d7764cfde79cf8feb
anak10thn <anak10thn@gmail.com>
2016-12-17 18:53:20 +0700

    UPDATE: init

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if [ -z "$CMDDOCK" ]
 then
   if [ ! -f "/opt/od/config.json" ]
   then
-    cat /opt/od/config.json.example | sed "s/db_id/root/g" | sed "s|your_password|pass123|g" | sed "s/localhost/oddb/g" > /opt/od/config.json
+    cat /opt/od/config.json.example | sed "s|your_username|root|g" | sed "s|your_password|pass123|g" | sed "s/localhost/oddb/g" > /opt/od/config.json
     python manage.py db init
     python manage.py db migrate
     python manage.py db upgrade
